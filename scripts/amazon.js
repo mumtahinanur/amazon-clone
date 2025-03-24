d84f86b974af061c9615ca7d3dfc966f2e6b2f0c
Mumtahina Nur <mumtahinanur9@gmail.com>
2025-03-24 20:39:28 +0600

    added the message 'added' to add to cart button

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,7 +45,8 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart 
+      js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -59,6 +60,26 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
+
+const addedMessageTimeouts = {};
+
+function renderAddedMessage(productId) {
+  const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+
+  addedMessage.classList.add('added-to-cart-visible');
+
+  const previousTimeOutId = addedMessageTimeouts[productId];
+
+  if (previousTimeOutId) {
+    clearTimeout(previousTimeOutId);
+  }
+
+  const timeOutId = setTimeout(() => {
+    addedMessage.classList.remove('added-to-cart-visible');
+  }, 2000);
+
+  addedMessageTimeouts[productId] = timeOutId;
+}
 
 function updateCartQuantity() {
   let cartQuantity = 0;
@@ -77,5 +98,6 @@ document.querySelectorAll('.js-add-to-cart')
       const {productId} = button.dataset;
       addToCart(productId);
       updateCartQuantity();
+      renderAddedMessage(productId);
     });
   });
