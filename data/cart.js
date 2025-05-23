14fd82c669a4c55d609e2857695d958ed6d2e06f
Mumtahina Nur <mumtahinanur9@gmail.com>
2025-05-23 13:27:13 +0600

    create tests for update delivery option

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,5 @@
+import {validDeliveryOption} from './deliveryOptions.js';
+
 export let cart;
 
 loadFromStorage();
@@ -95,6 +97,14 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
       matchingItem = cartItem;
     }
   });
+
+  if (!matchingItem) {
+    return;
+  }
+
+  if (!validDeliveryOption(deliveryOptionId)) {
+    return;
+  }
 
   matchingItem.deliveryOptionId = deliveryOptionId;
 
