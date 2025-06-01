a542fd9e6e68d76d2bb735304cff95a83204e4ab
Mumtahina Nur <mumtahinanur9@gmail.com>
2025-06-01 14:41:48 +0600

    load products from the backend

--- data/products.js
+++ data/products.js
@@ -80,6 +80,32 @@ export class Appliance extends Product {
   }
 }
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'appliance') {
+        return new Appliance(productDetails);
+      }
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -780,3 +806,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
